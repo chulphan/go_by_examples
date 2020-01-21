cc535736c375241ce2a02d628c249d5b8a866568
chulphan <chkim100617@gmail.com>
2020-01-21 18:29:16 +0900

    map 작업중

---
+++ maps/maps.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+	Maps are Go's built-in associative data type (sometimes called hashes or dicts in other language).
+	Map은 Go의 내장된 결합된 데이터 타입이다.
+*/
+
+func main() {
+	/*
+		To create an empty map, use the builtin make: make(map[key-type]val-type)
+	*/
+}
